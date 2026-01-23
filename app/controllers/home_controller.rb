8fad261b3244fae89ef7b3fa088db976702356ed
Airton <airton.junior@pinho.com.br>
2026-01-23 13:44:17 -0300

    Fix: correção de ordenamento de pagamentos

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,32 +2,53 @@ class HomeController < ApplicationController
   def index
     @start_date = Date.today.beginning_of_month.to_s
     @end_date = Date.today.to_s
-    
+
     client = InterClient.new
-    
+
     # 1. Get Initial Balance (1st of month)
     # The API returns positional balance if date is provided.
     initial_balance_data = client.get_balance(@start_date)
-    @initial_balance = initial_balance_data['disponivel'].to_f rescue 0.0
-    
+    @initial_balance = initial_balance_data["disponivel"].to_f rescue 0.0
+
     # 2. Get Current Balance
     current_balance_data = client.get_balance # no date = current
-    @current_balance = current_balance_data['disponivel'].to_f rescue 0.0
-    
+    @current_balance = current_balance_data["disponivel"].to_f rescue 0.0
+
     # 3. Get Monthly Transactions for summary
     @statement_data = client.get_statement(@start_date, @end_date)
-    
-    if @statement_data && @statement_data['transacoes']
-      transactions = @statement_data['transacoes']
-      @total_inflow = transactions.select { |t| t['tipoOperacao'] == 'C' }.sum { |t| t['valor'].to_f }
-      @total_outflow = transactions.select { |t| t['tipoOperacao'] == 'D' }.sum { |t| t['valor'].to_f }
-      @transaction_count = transactions.size
-    else
-      @total_inflow = 0.0
-      @total_outflow = 0.0
-      @transaction_count = 0
-    end
-    
+
+   @statement_data = client.get_statement(@start_date, @end_date)
+
+   if @statement_data && @statement_data["transacoes"]
+    transactions = @statement_data["transacoes"]
+
+    # Ordena da mais recente para a mais antiga
+    transactions = transactions.sort_by do |t|
+      begin
+        Date.parse(t["dataEntrada"])
+      rescue
+        Date.new(1900, 1, 1)
+      end
+    end.reverse
+
+    # Atualiza no hash já ordenado
+    @statement_data["transacoes"] = transactions
+
+    # Totais
+    @total_inflow = transactions.select { |t| t["tipoOperacao"] == "C" }
+                                .sum { |t| t["valor"].to_f }
+
+    @total_outflow = transactions.select { |t| t["tipoOperacao"] == "D" }
+                                  .sum { |t| t["valor"].to_f }
+
+    @transaction_count = transactions.size
+   else
+    @total_inflow = 0.0
+    @total_outflow = 0.0
+    @transaction_count = 0
+   end
+
+
     # 4. Debts summary
     @debts = Debt.where(date: Date.today.beginning_of_month..Date.today.end_of_month).or(Debt.where(date: nil))
     @total_debts = @debts.sum(:value)
